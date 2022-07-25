81001c9fd75336576b2d61180e5e606cacc0a989
Andrew Wong <andrew.j.wong@outlook.com>
2022-07-26 05:27:52 +1000

    Update adbd

--- misc/adbd_launcher/adbStart.py
+++ misc/adbd_launcher/adbStart.py
@@ -1,4 +1,5 @@
 import os
+import sys
 def run(cmd):
     return os.popen(cmd).read().strip()
 
@@ -8,7 +9,21 @@ serial = "8a80ab8936d76c118000"
 challenge = run(f"adb shell {vinda}rockrobo dynamickey")
 response = run(f"vinda.exe {serial} {challenge}").split("\n")[1].strip()
 print(f"{challenge=} {response=}")
-os.system(f"adb shell {vinda}{response}touch /tmp/a")
+
+if len(sys.argv) > 1:
+    command = " ".join(sys.argv[1:])
+else:
+    command = "dynamickey"
+
+has_invalid_char = False
+for invalid_char in ";&|`":
+    if invalid_char in command:
+        has_invalid_char = True
+        print("Error | Invalid character found: " + invalid_char)
+if not has_invalid_char:
+    cmd = f"adb shell \"{vinda}{response} {command}\""
+    print(f"{cmd=}")
+    os.system(cmd)
 
 # 1062 no
 # 1060 nah?
